ee9bfb12445a8603f526b38f4176d828022301a4
Tiffany White <tiffanywhitedev412@gmail.com>
2016-01-21 23:50:06 -0500

    Updates assignment, adds IDEA files to .gitignore

---
+++ Schmoopy's/Bookstore.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+public class Bookstore {
+
+    public static void main(String[] args) {
+
+        boolean custormers = false;
+
+        Scanner keyboard = new Scanner(System.in);
+
+
+        do {
+            System.out.println("Is there another customer in line? Press 1 for yes and 2 for no.");
+            int moreCustomers = keyboard.nextInt();
+
+            if (moreCustomers == 1) {
+                
+            }
+        }
+
+    }
+}
