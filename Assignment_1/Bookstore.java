09ce425a40d6f5188756cc44857f0e08961839b5
Tiffany White <tiffanywhitedev412@gmail.com>
2016-01-24 20:50:32 -0500

    Adds more code to Bookstore file

--- Assignment_1/Bookstore.java
+++ Assignment_1/Bookstore.java
@@ -5,21 +5,25 @@ public class Bookstore {
 
     public static void main(String[] args) {
 
-        boolean custormers = false;
+        boolean customers = false;
 
         Scanner keyboard = new Scanner(System.in);
 
+        System.out.println("Is there another customer in line? Press 1 for yes and 2 for no.");
+        int moreCustomers = keyboard.nextInt();
 
-        do {
-            System.out.println("Is there another customer in line? Press 1 for yes and 2 for no.");
-            int moreCustomers = keyboard.nextInt();
-
-            if (moreCustomers == 1) {
-                custormers = true;
-            } else {
-                System.exit(0);
-            }
+        if (moreCustomers == 1) {
+            customers = true;
+        } else {
+            System.exit(0);
         }
+        while (true) {
+           System.out.println("Books are $5 each. Bookmarks are $1 or a pack of 6 for $5, and painting " +
+           "of books are $100 each. There is a tax of 7%.");
+           System.out.println("What would you like to order today?");
 
+            String order = keyboard.nextLine();
+            
+        }
     }
 }
