dee8f60aec454650d177e003b812e53cc2b94d5b
Tiffany White <tiffanywhitedev412@gmail.com>
2016-01-29 21:06:38 -0500

    Redoes loop and nested conditionals. Kinda lost atm

--- Assignment_1/Bookstore.java
+++ Assignment_1/Bookstore.java
@@ -5,29 +5,35 @@ public class Bookstore {
 
     public static void main(String[] args) {
 
-        boolean customers = false;
-
         Scanner keyboard = new Scanner(System.in);
 
-        System.out.println("More customers in line? Press 1 for yes and 2 for no. > ");
-        int moreCustomers = keyboard.nextInt();
+        boolean customers = false;
 
         int numBooks = 0;
         int numBookmarks = 0;
         int numPaintings = 0;
 
-        if (moreCustomers == 1) {
-            System.out.println("Books are $5 each. Bookmarks are $1 or a pack of 6 for $5, and painting " +
-                    "of books are $100 each. There is a tax of 7%.");
-            if (moreCustomers == 1) {
+
+        do {
+            System.out.print("Is there another customer? Press 1 for Yes and 2 for No > ");
+            int moreCustomers = keyboard.nextInt();
+            
+            if (moreCustomers == 2) {
+                customers = false;
+            } else if (moreCustomers == 1) {
+                System.out.println("Books are $5 each. Bookmarks are $1 or a pack of 6 for $5, and painting " +
+                        "of books are $100 each. There is a tax of 7%.");
+                customers = true;
+            } else if (moreCustomers == 1) {
                 System.out.println("What would you like to order today?");
                 String order = keyboard.nextInt();
                 System.out.println("Currently in cart: " + order);
+                customers = true;
+            } else {
+                System.out.println("Enter 1 or 2!");
+                customers = true;
             }
 
-
-        } else {
-            System.exit(0);
-        }
+        } while (customers);
     }
 }
