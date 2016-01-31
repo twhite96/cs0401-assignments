9de2f3f10bb6f5ef8d846290f0c5bd77fc08ac56
Tiffany White <tiffanywhitedev412@gmail.com>
2016-01-30 23:34:58 -0500

    Adds methods

--- Assignment_1/Bookstore.java
+++ Assignment_1/Bookstore.java
@@ -5,14 +5,15 @@ public class Bookstore {
 
     public static void main(String[] args) {
 
+        customers();
+        showInventory();
+        order();
+
+    public int customers() {
+
         Scanner keyboard = new Scanner(System.in);
 
         boolean moreCustomers = false;
-
-        int numBooks = 0;
-        int numBookmarks = 0;
-        int numPaintings = 0;
-
 
         do {
             System.out.print("Is there another customer? Press 1 for Yes and 2 for No > ");
@@ -21,13 +22,6 @@ public class Bookstore {
             if (foo == 2) {
                 moreCustomers = false;
             } else if (foo == 1) {
-                System.out.println("Books are $5 each. Bookmarks are $1 or a pack of 6 for $5, and painting " +
-                        "of books are $100 each. There is a tax of 7%.");
-
-                System.out.println("What would you like to order today? Please enter a valid option, 1 through 5 > ");
-
-                String order = keyboard.nextInt();
-                System.out.println("Currently in cart: " + order);
                 moreCustomers = true;
             } else {
                 System.out.println("Enter 1 or 2!");
@@ -35,5 +29,23 @@ public class Bookstore {
             }
 
         } while (moreCustomers);
+
+    public void showInventory() {
+
+        System.out.println("What would you like to order today?" + "1 - Buy Books - $5.00 each" + "2 - Buy Bookmarks - $1 each, $5 for six-pack" +
+                "Buy Paintings of Books - $100 each" + "4 - See current order" + "5 Checkout");
+
+    public int order() {
+        Scanner keyboard = new Scanner(System.in);
+
+        int numBooks = 0;
+        int numBookmarks = 0;
+        int numPaintings = 0;
+
+        numBooks = keyboard.nextInt();
+
+    }
     }
 }
+
+
