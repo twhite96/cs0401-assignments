1ce0f0915db5c625bc18254202e829de4460df62
Tiffany White <tiffanywhitedev412@gmail.com>
2016-02-02 16:31:12 -0500

    Got it to run but not right.

--- Assignment_1/Bookstore.java
+++ Assignment_1/Bookstore.java
@@ -13,27 +13,28 @@ public class Bookstore {
         // I create a new point of sale
         Bookstore b = new Bookstore();
         // start selling things
-
-        b.startSelling();
         
-        // 2 - show inventory
-        b.showInventory();
-    }
-
-    public static void startSelling() {
+        boolean customer;
         
-        Bookstore b = new Bookstore();
-        
-        boolean customer = false;
-        do {
             // 1 - Create a new cart
-            Cart cart = new Cart();
+         Cart cart = new Cart();
             
-            customer = b.moreCustomers();
+         customer = b.moreCustomers();
+            
+         b.showInventory();
+         b.chooseFromMenu();
+         
+         while(b.moreCustomers()) {
+            
+              cart.addABook();
+              cart.removeABook();
+              cart.addBookmarks();
+              cart.removeBookmarks();
+              cart.addPaintings();
+              cart.removeAPainting();
         }
-        //continues while there are more customers to be served
-        while (false);
     }
+
 
     public boolean moreCustomers() {
         System.out.println("Is there another customer?");
@@ -75,4 +76,52 @@ public class Bookstore {
         System.out.println("4 - See current order");
         System.out.println("5 - Checkout");
     }
+    
+    public int chooseFromMenu() {
+        System.out.println("What would you like to order today?");
+        System.out.println("1 - Buy Books - $5.00 each");
+        System.out.println("2 - Buy Bookmarks - $1 each, $5 for six-pack");
+        System.out.println("3 - Buy Paintings of Books - $100 each");
+        System.out.println("4 - See current order");
+        System.out.println("5 - Checkout");
+        
+        boolean validAnswer = false;
+        int choice = keyboard.nextInt();
+        do {
+            // Check what customer has said
+            int input = keyboard.nextInt();
+            switch (input) {
+                //check what the input is
+                case 1:
+                    // They said 1
+                    validAnswer = true;
+                    System.out.println("How many books do you want to buy? > " + choice);
+                    break;
+                case 2:
+                    // They said 2
+                    validAnswer = true;
+                    System.out.println("How many bookmarks do you want to buy? > " + choice);
+                    break;
+                case 3:
+                    validAnswer = true;
+                    System.out.println("How many paintings do you want to buy? > " + choice);
+                    break;
+                case 4:
+                    validAnswer = true;
+                    System.out.println("Your current order is > " + choice);
+                    break;
+                case 5:
+                    validAnswer = true;
+                    System.out.println("Checkout.");
+                    break;
+                default:
+                    // They said something else than 1, 2, 3, 4, or 5
+                    validAnswer = false;
+                    break;
+            }
+        }
+              while (choice == 1 || choice == 2 || choice == 3 || choice == 4 || choice == 5);
+             return choice;
+    }
 }
+        
