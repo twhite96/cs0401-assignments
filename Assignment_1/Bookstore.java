e04de33f8aa97ed75908da1ee1c7f92a3b9653eb
Tiffany White <tiffanywhitedev412@gmail.com>
2016-02-01 21:22:32 -0500

    Not sure what is happening here

--- Assignment_1/Bookstore.java
+++ Assignment_1/Bookstore.java
@@ -1,40 +1,72 @@
 import java.util.Scanner;
 
-
 public class Bookstore {
-    
-     Scanner keyboard;
-     
-    this.keyboard = new Scanner(System.in);
-    
+
+    Scanner keyboard;
+
+    public Bookstore() {
+        // initialise my scanner
+        this.keyboard = new Scanner(System.in);
+    }
+
     public static void main(String[] args) {
+        // I create a new point of sale
+        Bookstore b = new Bookstore();
+        // start selling things
+
+        b.startSelling();
+    }
+
+    public static void startSelling() {
         
         Bookstore b = new Bookstore();
-        customers();
-        b.showInventory();
-        order();
-
-    public int customers() {
-
-        boolean moreCustomers = false;
-
+        
+        boolean customer;
         do {
-            System.out.print("Is there another customer? Press 1 for Yes and 2 for No > ");
-            int foo = keyboard.nextInt();
+            // 1 - Create a new cart
+            Cart cart = new Cart();
+            // 2 - show inventory
+            b.showInventory();
 
-            if (foo == 2) {
-                moreCustomers = false;
-            } else if (foo == 1) {
-                moreCustomers = true;
-            } else {
-                System.out.println("Enter 1 or 2!");
-                moreCustomers = true;
+            customer = b.moreCustomers();
+        }
+        //continues while there are more customers to be served
+        while (customer);
+    }
+
+    private boolean moreCustomers() {
+        System.out.println("Is there another customer?");
+        System.out.println("Press 1 for Yes and 2 for No > ");
+        boolean validAnswer = false;
+        boolean answer = false;
+        do {
+            // Check what customer has said
+            int input = keyboard.nextInt();
+            switch (input) {
+                //check what the input is
+                case 1:
+                    // They said 1
+                    validAnswer = true;
+                    answer = true;
+                    break;
+                case 2:
+                    // They said 2
+                    validAnswer = true;
+                    answer = false;
+                    break;
+                default:
+                    // They said something else than 1 or 2
+                    validAnswer = false;
+                    break;
             }
-
-        } while (moreCustomers);
+        }
+        //continue to ask the question till a valid answer has been given
+        while (!validAnswer);
+        // We return the answer
+        return answer;
+    }
 
     public void showInventory() {
-
         System.out.println("What would you like to order today?");
         System.out.println("1 - Buy Books - $5.00 each");
         System.out.println("2 - Buy Bookmarks - $1 each, $5 for six-pack");
@@ -42,31 +74,4 @@ public class Bookstore {
         System.out.println("4 - See current order");
         System.out.println("5 - Checkout");
     }
-
-    public int order() {
-        
-        int numBooks = 0;
-        int numBookmarks = 0;
-        int numPaintings = 0;
-        
-        System.out.println("What would you like to order? Please enter a valid option (1 through 5) > ");
-        
-        int input = keyboard.nextInt();
-        
-        if (input == 1) {
-            System.out.println("How many books would you like to order? > ");
-            
-             numBooks = keyboard.nextInt();
-        } else if (input == 2) {
-            System.out.println("How many bookmarks would you like to order? > ");
-            
-            numBookmarks = keyboard.nextInt();
-        } else if (input == 3) {
-            System.out.println("How many paintings would you like to order? > ");    
-        }
-        
-
-        
-
-        
-        
+}
---
+++ Assignment_1/Cart.java
@@ -0,0 +1,47 @@
+/**
+ * Created by tiffanywhite on 2/1/16.
+ */
+public class Cart {
+    int numBooks = 0;
+    int numBookmarks = 0;
+    int numPaintings = 0;
+
+    public int addABook() {
+        numBooks++;
+        return numBooks;
+    }
+
+    public int removeABook() {
+
+        if(numBooks > 0) {
+            numBooks--;
+            return numBooks;
+        }
+    }
+
+    public int addBookmarks() {
+        numBookmarks++;
+        return numBookmarks;
+    }
+
+    public int removeBookmarks() {
+        if (numBookmarks > 0) {
+            numBookmarks--;
+            return numBookmarks;
+        }
+    }
+
+    public int addPaintings() {
+        numPaintings++;
+        return numPaintings;
+    }
+
+    public int removeAPainting() {
+        if (numPaintings > 0) {
+            numPaintings--;
+            return numPaintings;
+        }
+    }
+}
+
+
