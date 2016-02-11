f3b182b9b07e7ef7c166ad8d32edebc2e642d1d0
Tiffany White <tiffanywhitedev412@gmail.com>
2016-02-11 01:54:50 -0500

    Adds java files

---
+++ Assignment_2/Player.java
@@ -0,0 +1,12 @@
+import java.io*;
+import.java.util.Scanner;
+
+public class Player {
+    public static void main(String[] args) throws IOException {
+        
+    }
+    public static void createFile(String filename) throws IOException {
+    File player = new File(fileName);
+    Scanner keyboard = new Scanner(player);
+    }
+ 
