cb6f54e0c78a228a2d1d2e3fecadd622064313ad
Tiffany White <tiffanywhitedev412@gmail.com>
2016-02-18 23:30:08 -0500

    Updates Player class with getters and setters

--- Assignment_2/Player.java
+++ Assignment_2/Player.java
@@ -2,9 +2,16 @@ import java.io*;
 import.java.util.Scanner;
 
 public class Player {
-    public static void main(String[] args) throws IOException {
-        
+       private int handsPlayed;
+       private double money;
+       private int handsWon;
+
+    public Player () {
+        handsPlayed = 0;
+        money = 0.00;
+        handsWon = 0;
     }
+    
     public static void createFile(String filename) throws IOException {
         Scanner sc = new Scanner(System.in);
         System.out.println("What your name? > ");
@@ -15,4 +22,27 @@ public class Player {
             file.createNewFile(input);   
         }
     }
+    
+     public static void writeToFile()
+     public void setHandsPlayed(int handsPlayed) {
+         this.handsPlayed += handsPlayed;
+     }
+     
+     public int getHandsPlayed() {
+         return handsPlayed;
+     }
+     public void setMoney(double money) {
+         this.money += money;
+     }
+     public double getMoney() {
+         return money;
+     }
+     public void getHandsWon (int handsWon) {
+         this.handsWon += handsWon;
+     }
+     public int getHandsWon() {
+         return handsWon;
+     }
+
+}
  
